c0047beba5a856e1bf2930c61f4e606fe41c7d67
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-03-01 19:25:30 +0530

    this is to change the font weight

--- src/components/unloaded/Topnav.tsx
+++ src/components/unloaded/Topnav.tsx
@@ -4,7 +4,7 @@ const Topnav=()=>{
     const[query,setquery]=useState("");
     return ( 
         <>
-        <div className="w-[100%] h-[10vh] text-zinc-200 relative flex justify-center items-center">
+        <div className="w-[100%] h-[10vh] text-zinc-200 font-bold relative flex justify-center items-center">
             <i className="ri-search-2-line text-3xl text-zinc-400 "></i>
             <input onChange={(e)=>{setquery(e.target.value)}} value={query} type="text" className="w-[50%] text-zinc-200 border-none outline-none bg-transparent text-xl mx-10 p-5" placeholder="search anything"/>
             {query.length>0 && <i onClick={()=>{setquery("")}} className="ri-close-circle-fill text-3xl text-zinc-400 hover:cursor-pointer"></i>}
